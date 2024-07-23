358f9a54956fd72fd79dbfc5357b0f6760006777
SahajA25 <143540034+SahajA25@users.noreply.github.com>
2024-07-24 00:11:32 +0530

    Add files via upload

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Portfolio from './Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
